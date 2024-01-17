0a5f02e45ed29dcf8e9638513c25cab000520b6a
Carl Molina <carlandrew.molina@gmail.com>
2024-01-16 16:23:30 -0800

    Debugged left and right arrow function, before first code review

--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -38,6 +38,73 @@ it("works when you click on the right arrow", function() {
   ).toBeInTheDocument();
 });
 
+
+// expect the second image to move to first image on left arrow
+it("works when you click on the left arrow", function() {
+  const { container } = render (
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+});
+
+it("should hide left arrow when on first image", function() {
+  const { container } = render (
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  expect(
+    container.querySelector('.bi-arrow-left-circle')
+  ).not.toBeInTheDocument();
+});
+
+
+
+it("should hide right arrow when on last image", function() {
+  const { container } = render (
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  expect(
+    container.querySelector('.bi-arrow-right-circle')
+  ).not.toBeInTheDocument();
+})
+
 //SNAPSHOT TEST
 it("matches snapshot", function(){
   const { container } = render(
